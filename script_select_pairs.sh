3ef9c48d9dd82d7f6aa948e1de79bb2ef20d4051
dbguilherme <dbguilherme@gmail.com>
2020-03-25 17:14:04 -0300

    active learning

--- script_select_pairs.sh
+++ script_select_pairs.sh
@@ -85,6 +85,7 @@
     flag=0
     temp=0
     mid=$len
+    new_mid=0
     mid=$(($mid-1)) #start from zero 
     for i in $(seq $mid -1 0 ); do    
     	j=$(($j+1))
@@ -96,7 +97,7 @@
         cat $name | sort > temp
         cat x_negat.* x_posit.* | cut -d' ' -f2 | sort | join - temp -v2 | sort > clean_data #data without already labeled docs 
        echo "script prune pairs..."
-         .././script_prune_pairs.sh clean_data  $j $TOPIC $inicialize_allac $rules >  /tmp/lixo.$TOPIC
+         .././script_prune_pairs.sh clean_data  $j $TOPIC $inicialize_allac $rules $sliding_windows >  /tmp/lixo.$TOPIC
 	inicialize_allac=3
         cat /tmp/lixo.$TOPIC
         
@@ -150,7 +151,7 @@
             cat $name | sort > temp
             cat x_negat.* x_posit.* | cut -d' ' -f2 | sort | join - temp -v2 | sort > new_data
             #echo "positivos no seed `cat $name | sort | join - posit_file -2 2  | wc -l `"
-            .././script_prune_pairs.sh new_data  $j $TOPIC $inicialize_allac $rules &> /tmp/lixo.$TOPIC  
+            .././script_prune_pairs.sh new_data  $j $TOPIC $inicialize_allac $rules $sliding_windows &> /tmp/lixo.$TOPIC  
             cat /tmp/lixo.$TOPIC  
             pos=`grep "docs positivos coletados" /tmp/lixo.$TOPIC | cut -d' ' -f4`
             neg=`grep "docs positivos coletados" /tmp/lixo.$TOPIC | cut -d' ' -f11`      
