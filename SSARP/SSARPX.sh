bc0ac6737e9e008c290737a6371ac92cc8c6d63e
mvtodescato <matheus.todescato@estudante.uffs.edu.br>
2020-05-18 13:00:49 -0300

    Try/catch commented

--- SSARP/SSARPX.sh
+++ SSARP/SSARPX.sh
@@ -4,7 +4,7 @@
 numfeatures=10 # numero de features no treino / teste
 binsfrom=10 # numero de bins (de) 
 binsto=10 # numero de bins (at
-
+source "${ABS_PATH}/handle_errors"
 treina_arff=$1".arff"
 
 file_original=$2
@@ -18,13 +18,19 @@ rules=$8
 
 echo "the file have "$numfeatures;
 
+#try
+#(
+    rm -r result_temp_lac_train_TUBEfinal.txt/ 
+    rm -r result_temp_lac_train_TUBE*
+        
+    rm train_nohead.arff  
+    echo "gerando bins ../.././gera_bins_TUBE.sh $treina_arff  $numfeatures $binsfrom $binsto  " 
+    ../.././gera_bins_TUBE.sh $treina_arff  $numfeatures $binsfrom $binsto
+#) 2> $STD_ERROR_OUT
 
-rm -r result_temp_lac_train_TUBEfinal.txt/ 
-rm -r result_temp_lac_train_TUBE*
-    
-rm train_nohead.arff  
-echo "gerando bins ../.././gera_bins_TUBE.sh $treina_arff  $numfeatures $binsfrom $binsto  " 
-../.././gera_bins_TUBE.sh $treina_arff  $numfeatures $binsfrom $binsto   
+#catch || {
+#    kill  -SIGUSR2 -SIGUSR1 `ps --pid $$ -oppid=`; exit_on_error
+#}   
 
 
 # 
@@ -68,19 +74,25 @@ echo ../../discretize_TUBE.pl train-$suffix train_nohead.arff $numfeatures  lac_
 
 if [ $vez -eq 10 ]; 
 then
-    echo "removendo o arquivo bkp anterior "
-    rm  bkppairs.$topic 
-    rm alac_full_lac_train_TUBEfinal.txt.$topic 
-    rm alac_lac_train_TUBEfinal.txt.$topic
-    echo "iniciando o seed"
+    #try
+    #(
+        echo "removendo o arquivo bkp anterior "
+        rm  bkppairs.$topic 
+        rm alac_full_lac_train_TUBEfinal.txt.$topic 
+        rm alac_lac_train_TUBEfinal.txt.$topic
+        echo "iniciando o seed"
 
-    echo "discretizando seed ssarp"
-    ../../discretize_TUBE.pl train-$suffix $seed_ssarp $numfeatures  lac_train_TUBE_seed.txt.$topic 
-    ./updateRows.pl lac_train_TUBE_seed.txt.$topic  lac_train_TUBEfinal_seed.txt.$topic   $(($vez-1))
-    
-    cat lac_train_TUBEfinal_seed.txt.$topic  >> alac_full_lac_train_TUBEfinal.txt.$topic 
-    cat lac_train_TUBEfinal_seed.txt.$topic  >> alac_lac_train_TUBEfinal.txt.$topic 
-    
+        echo "discretizando seed ssarp"
+        ../../discretize_TUBE.pl train-$suffix $seed_ssarp $numfeatures  lac_train_TUBE_seed.txt.$topic 
+        ./updateRows.pl lac_train_TUBE_seed.txt.$topic  lac_train_TUBEfinal_seed.txt.$topic   $(($vez-1))
+        
+        cat lac_train_TUBEfinal_seed.txt.$topic  >> alac_full_lac_train_TUBEfinal.txt.$topic 
+        cat lac_train_TUBEfinal_seed.txt.$topic  >> alac_lac_train_TUBEfinal.txt.$topic 
+    #) 2> $STD_ERROR_OUT
+
+    #catch || {
+    #    kill -SIGUSR2 -SIGUSR1 `ps --pid $$ -oppid=`; exit_on_error
+    #}
 fi
 
 echo " roda o ALAC $numfeatures" 
@@ -96,15 +108,21 @@ done
 
 #junta as instancias selecionadas em cada particao em um arquivo unico contendo todas as features
 echo "Gerando o treino a partir das instancias selecionadas em cada particao.."
+#try
+#(
+    cp alac_lac_train_TUBEfinal.txt.$topic   bkppairs.$topic 
+    cat lac_train_TUBEfinal_seed.txt.$topic  |   awk '{ print $1 }' |  while read instance; do  sed -i  "/^$instance /d"  bkppairs.$topic ;  done
 
-cp alac_lac_train_TUBEfinal.txt.$topic   bkppairs.$topic 
-cat lac_train_TUBEfinal_seed.txt.$topic  |   awk '{ print $1 }' |  while read instance; do  sed -i  "/^$instance /d"  bkppairs.$topic ;  done
+    ./scriptRemoveRows.pl alac_full_lac_train_TUBEfinal.txt.$topic  composite_train_uniqB$numfeatures composite_train_uniqBold$numfeatures $vez
 
-./scriptRemoveRows.pl alac_full_lac_train_TUBEfinal.txt.$topic  composite_train_uniqB$numfeatures composite_train_uniqBold$numfeatures $vez
+    cat composite_train_uniqB$numfeatures  | awk '{ print $1 }'  | while read instance; do  sed  -n  "$instance"p  $file_original; done > /tmp/final_treina.arff.$vez.$topic;
 
-cat composite_train_uniqB$numfeatures  | awk '{ print $1 }'  | while read instance; do  sed  -n  "$instance"p  $file_original; done > /tmp/final_treina.arff.$vez.$topic;
+    cut -d' ' -f1 /tmp/final_treina.arff.$vez.$topic > /tmp/label.$vez.$topic
+    echo "gerando arquivo SVM treino.."
+    cut -d' ' -f1 /tmp/final_treina.arff.$vez.$topic >  /tmp/outSSARP.$vez.$topic
+#) 2> $STD_ERROR_OUT
 
-cut -d' ' -f1 /tmp/final_treina.arff.$vez.$topic > /tmp/label.$vez.$topic
-echo "gerando arquivo SVM treino.."
-cut -d' ' -f1 /tmp/final_treina.arff.$vez.$topic >  /tmp/outSSARP.$vez.$topic
+#catch || {
+ #   kill -SIGUSR2 -SIGUSR1 `ps --pid $$ -oppid=`; exit_on_error
+#}
  
