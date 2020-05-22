a5cd606f06b6dbc558d0f93e9c61a06a4c123571
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2020-05-22 18:57:08 -0300

    fix: Adds only doc_ids from round 1.

--- data-extraction-scripts/doc_uid_mapping.py
+++ data-extraction-scripts/doc_uid_mapping.py
@@ -7,15 +7,7 @@ base, and the second column is the document name
 to be used in the SCAL format.
 """
 
-import pandas
 import os
-
-data = pandas.read_csv("metadata.csv")
-
-uids = data.cord_uid.tolist()
-
-print("Total columns:", len(uids))
-print("Unique uids:", len(set(uids)))
 
 repeated = []      # list of repeated (used just for information)
 
@@ -25,18 +17,24 @@ doc_mapping = {}   # maps names (could be used for uniqueness check)
 if os.path.exists("topic.id.mapping"):
     os.remove("topic.id.mapping")
 
-with open("topic.id.mapping", "a") as map_file:
-    for elem in uids:
-        if elem in doc_mapping.keys():
-            repeated.append(elem)
+with open("topic.id.mapping", "a") as map_file, open("docids-rnd1.txt", "r") as valid_uids_file:
+    valid_uids = valid_uids_file.readlines()
+
+    print("Total of elements:", len(valid_uids))
+    print("Unique elements:", len(set(valid_uids)))
+
+    for _uid in valid_uids:
+        uid = _uid.strip()
+        if uid in doc_mapping.keys():
+            repeated.append(uid)
 
         else:
-            doc_mapping[elem] = str(unique_count).zfill(7)
+            doc_mapping[uid] = str(unique_count).zfill(7)
             unique_count += 1
 
-            map_file.write(" ".join([elem, doc_mapping[elem]]) + "\n")
+            map_file.write(" ".join([uid, doc_mapping[uid]]) + "\n")
 
-        doc_name = doc_mapping[elem]
+        doc_name = doc_mapping[uid]
 
-print("Repeated elemets found:", len(repeated))
-print("Unique repeated elements:", len(set(repeated)))
+print("Repeated elements found:", len(repeated))
+print("Unique elements within the repeated:", len(set(repeated)))
--- data-extraction-scripts/extract_queries.py
+++ data-extraction-scripts/extract_queries.py
@@ -1,9 +1,13 @@
+import os
 import xml.etree.ElementTree as ET
 
 tree = ET.parse("topics-rnd1.xml")
 root = tree.getroot()
 
-with open("cord-19.topic.stemming.txt", "a") as cord_base_file:
+if os.path.exists("judgement/cord-19.topic.stemming.txt"):
+    os.remove("judgement/cord-19.topic.stemming.txt")
+
+with open("judgement/cord-19.topic.stemming.txt", "a") as cord_base_file:
     for elem in root:
         query = elem[0].text
         topic = "tr" + elem.attrib["number"]
