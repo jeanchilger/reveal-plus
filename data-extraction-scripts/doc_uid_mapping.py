04954d1030a1c75f67693aa12cb8983eb8856cec
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2020-05-28 18:22:48 -0300

    fix: Fixes mapping generation script.

--- data-extraction-scripts/doc_uid_mapping.py
+++ data-extraction-scripts/doc_uid_mapping.py
@@ -7,9 +7,15 @@ base, and the second column is the document name
 to be used in the SCAL format.
 """
 
+import pandas
 import os
 
 repeated = []      # list of repeated (used just for information)
+invalid = []       # list of invalid doc ids within metadata
+
+data = pandas.read_csv("metadata.csv")
+
+all_uids = data.cord_uid.tolist()
 
 unique_count = 0   # tracks document name
 doc_mapping = {}   # maps names (could be used for uniqueness check)
@@ -18,23 +24,26 @@ if os.path.exists("topic.id.mapping"):
     os.remove("topic.id.mapping")
 
 with open("topic.id.mapping", "a") as map_file, open("docids-rnd1.txt", "r") as valid_uids_file:
-    valid_uids = valid_uids_file.readlines()
+    valid_uids = [line.strip() for line in valid_uids_file.readlines()]
 
-    print("Total of elements:", len(valid_uids))
-    print("Unique elements:", len(set(valid_uids)))
+    for uid in all_uids:
 
-    for _uid in valid_uids:
-        uid = _uid.strip()
-        if uid in doc_mapping.keys():
-            repeated.append(uid)
+        if uid in valid_uids:
+
+            if uid in doc_mapping.keys():
+                repeated.append(uid)
+
+            else:
+                doc_mapping[uid] = str(unique_count).zfill(7)
+                unique_count += 1
+
+                map_file.write(" ".join([uid, doc_mapping[uid]]) + "\n")
 
         else:
-            doc_mapping[uid] = str(unique_count).zfill(7)
-            unique_count += 1
+            invalid.append(uid)
 
-            map_file.write(" ".join([uid, doc_mapping[uid]]) + "\n")
 
-        doc_name = doc_mapping[uid]
-
-print("Repeated elements found:", len(repeated))
+print("Repeated (valid) elements found:", len(repeated))
 print("Unique elements within the repeated:", len(set(repeated)))
+print("Invalid docs:", len(invalid))
+print("Valid docs:", len(doc_mapping.keys()))
