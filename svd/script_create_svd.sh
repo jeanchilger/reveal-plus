cf12cc3ff51934da87b2fbe023eb5f0b4b0a780d
guilherme <gui_bianco@yahoo.com.br>
2020-06-12 11:26:10 -0300

    adding tar file

--- svd/script_create_svd.sh
+++ svd/script_create_svd.sh
@@ -3,13 +3,23 @@ input=$1
 cp $input /tmp/athome1
 cat $2 >> /tmp/athome1
 pushd ../svd
-cut -d' ' -f2- /tmp/athome1 > /tmp/athome1_2
-cut -d' ' -f1 /tmp/athome1 > /tmp/labels
-sed -i 's/^/0 /' /tmp/athome1_2
 
-python3 trans.py /tmp/athome1_2  out 
+
+
+
+#cut -d' ' -f2- /tmp/athome1 > /tmp/athome1_2
+#cut -d' ' -f1 /tmp/athome1 > /tmp/labels
+#sed -i 's/^/0 /' /tmp/athome1_2
+
+#python3 trans.py /tmp/athome1_2  out 
+python3 trans.py <(sed  's/^/0 /' <(cut -d' ' -f2- /tmp/athome1))  out
 
 sed -i 's/^0//' out
-paste /tmp/labels out -d ' ' | sed 's/  / /'  > $input.svd
+
+paste <(cut -d' ' -f1 /tmp/athome1) out -d ' ' | sed 's/  / /'  > $input.svd
 popd
 echo "file $input.svd was created"
+
+
+
+
