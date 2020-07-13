1e771a5dc1fde9c9468c6aaa2c5ebbcb7719e879
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2020-07-13 16:29:19 -0300

    refactor: Fix identantion and spacing.

--- utils/script_prune_pairs.sh
+++ utils/script_prune_pairs.sh
@@ -1,44 +1,68 @@
-echo "↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓"
-
 TOPIC=$3
 N=$2
 flag=$4
 rules=$5
 
 echo "starting run ssarpx\n"
-#identify docs pos and neg
-cat $1 | sort | uniq | join - goldendb | cut -d' ' -f1 | sed -e 's/^/1 /' > temp_posit.$N.$TOPIC
-cat $1 | sort | uniq | join - goldendb -v1 | cut -d' ' -f1 | sed -e 's/^/-1 /' > temp_negat.$N.$TOPIC
+# identify docs pos and neg
 
-#produce the training set file used by SSARP
-cat temp_posit.$N.$TOPIC temp_negat.$N.$TOPIC |  sort -k2  | join - ../"$file".svm.fil.svd  -2 1 -1 2 > trainset.$N.$TOPIC
-cut -d ' ' -f2- trainset.$N.$TOPIC  > trainsetB.$N.$TOPIC
+cat $1 \
+    | sort \
+    | uniq \
+    | join - goldendb \
+    | cut -d' ' -f1 \
+    | sed -e 's/^/1 /' > temp_posit.$N.$TOPIC
+
+cat $1 \
+    | sort \
+    | uniq \
+    | join - goldendb -v1 \
+    | cut -d' ' -f1 \
+    | sed -e 's/^/-1 /' > temp_negat.$N.$TOPIC
+
+# produce the training set file used by SSARP
+cat temp_posit.$N.$TOPIC temp_negat.$N.$TOPIC \
+    | sort -k2 \
+    | join - ../"$file".svm.fil.svd  -2 1 -1 2 > trainset.$N.$TOPIC
+
+cut -d ' ' -f2- trainset.$N.$TOPIC > trainsetB.$N.$TOPIC
 python3 ../svd/convert_txt.py trainsetB.$N.$TOPIC trainset.$N.$TOPIC.arff goldendb
 
 cp seed_out.10.* seed_out
-#clean some files
+# clean some files
 cp trainset.$N.$TOPIC ../SSARP/run/
 cp seed_out ../SSARP/run/
 cp trainset.$N.$TOPIC.arff ../SSARP/run/
 cd ../SSARP/run/
 
-#run active learning
-./SSARPX.sh trainset.$N.$TOPIC trainset.$N.$TOPIC 50 $N seed_out $TOPIC $flag $rules &>   log_ssarp_stoping_$N
+# run active learning
+./SSARPX.sh trainset.$N.$TOPIC trainset.$N.$TOPIC 50 $N seed_out $TOPIC $flag $rules &> log_ssarp_stoping_$N
 
-#store result
+# store result
 cat label.$N.$TOPIC > /tmp/ssarp$N.$TOPIC
 cat log_ssarp_stoping_$N
 cd -
 mv /tmp/ssarp$N.$TOPIC .
 
+# compute docs positivos and negativos
+cat ssarp$N.$TOPIC \
+    | sort -k 2 \
+    | uniq \
+    | join - goldendb \
+    | cut -d' ' -f1 \
+    | sed -e 's/^/1 /' > x_posit_ssarp_end.$N
 
+cat ssarp$N.$TOPIC \
+    | sort -k 2 \
+    | uniq \
+    | join - goldendb -v1 \
+    | cut -d' ' -f1 \
+    | sed -e 's/^/-1 /' > x_negat_ssarp_end.$N
 
-#compute docs positivos and negativos
-cat ssarp$N.$TOPIC   | sort -k 2 | uniq | join - goldendb  | cut -d' ' -f1 | sed -e 's/^/1 /' > x_posit_ssarp_end.$N
-cat ssarp$N.$TOPIC   | sort -k 2 | uniq | join - goldendb -v1 | cut -d' ' -f1 | sed -e 's/^/-1 /' > x_negat_ssarp_end.$N
+cat x_posit_ssarp_end.$N x_negat_ssarp_end.$N \
+    | sort \
+    | uniq > ssarpout$N.$TOPIC
 
-
-cat x_posit_ssarp_end.$N x_negat_ssarp_end.$N | sort | uniq > ssarpout$N.$TOPIC
 cut -d' ' -f2  ssarpout$N.$TOPIC > sub_new_ssarp.$N.$TOPIC
 
 echo "docs positivos coletados `wc -l < x_posit_ssarp_end.$N`   docs negativos ----  `wc -l < x_negat_ssarp_end.$N`"
