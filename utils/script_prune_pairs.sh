d7013a06cd4884606c94e0db61d1e6a8e7deab74
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2020-07-13 16:33:59 -0300

    refactor: Change comments and prints.

--- utils/script_prune_pairs.sh
+++ utils/script_prune_pairs.sh
@@ -3,9 +3,11 @@ N=$2
 flag=$4
 rules=$5
 
-echo "starting run ssarpx\n"
-# identify docs pos and neg
+echo "Starting run ssarpx"
 
+#----------------------------------------------------------------------
+# identify positive and negative documents
+#----------------------------------------------------------------------
 cat $1 \
     | sort \
     | uniq \
@@ -20,7 +22,9 @@ cat $1 \
     | cut -d' ' -f1 \
     | sed -e 's/^/-1 /' > temp_negat.$N.$TOPIC
 
+#----------------------------------------------------------------------
 # produce the training set file used by SSARP
+#----------------------------------------------------------------------
 cat temp_posit.$N.$TOPIC temp_negat.$N.$TOPIC \
     | sort -k2 \
     | join - ../"$file".svm.fil.svd  -2 1 -1 2 > trainset.$N.$TOPIC
@@ -29,22 +33,31 @@ cut -d ' ' -f2- trainset.$N.$TOPIC > trainsetB.$N.$TOPIC
 python3 ../svd/convert_txt.py trainsetB.$N.$TOPIC trainset.$N.$TOPIC.arff goldendb
 
 cp seed_out.10.* seed_out
+
+#----------------------------------------------------------------------
 # clean some files
+#----------------------------------------------------------------------
 cp trainset.$N.$TOPIC ../SSARP/run/
 cp seed_out ../SSARP/run/
 cp trainset.$N.$TOPIC.arff ../SSARP/run/
 cd ../SSARP/run/
 
+#----------------------------------------------------------------------
 # run active learning
+#----------------------------------------------------------------------
 ./SSARPX.sh trainset.$N.$TOPIC trainset.$N.$TOPIC 50 $N seed_out $TOPIC $flag $rules &> log_ssarp_stoping_$N
 
+#----------------------------------------------------------------------
 # store result
+#----------------------------------------------------------------------
 cat label.$N.$TOPIC > /tmp/ssarp$N.$TOPIC
 cat log_ssarp_stoping_$N
 cd -
 mv /tmp/ssarp$N.$TOPIC .
 
-# compute docs positivos and negativos
+#----------------------------------------------------------------------
+# compute positive and negative documents
+#----------------------------------------------------------------------
 cat ssarp$N.$TOPIC \
     | sort -k 2 \
     | uniq \
@@ -65,4 +78,6 @@ cat x_posit_ssarp_end.$N x_negat_ssarp_end.$N \
 
 cut -d' ' -f2  ssarpout$N.$TOPIC > sub_new_ssarp.$N.$TOPIC
 
-echo "docs positivos coletados `wc -l < x_posit_ssarp_end.$N`   docs negativos ----  `wc -l < x_negat_ssarp_end.$N`"
+e_secondary "script_prune_pairs report:"
+echo -e "\tPositive collected ..... `wc -l < x_posit_ssarp_end.$N`"
+echo -e "\tNegative collected ..... `wc -l < x_negat_ssarp_end.$N`"
